4fcd4d452abad6e3b5347cc47a243924e9b9cbc9
Abhishek kushwah <113005763+abhishek42kushwah@users.noreply.github.com>
2024-04-08 11:19:32 +0530

    Change case of variable name "router" in User.js
    The variable name "router" was changed from lowercase to Uppercase in the User.js file. No functional changes were made to the code.

--- routes/Profile.js
+++ routes/Profile.js
@@ -6,20 +6,24 @@ const {
     deleteAccount,
     getAllUserDetails,
     updateDisplayPicture,
-    getEnrolledCourse} = require("../controllers/Profile");
-const {
-    auth } =require("../middilewares/auth");
+    getEnrolledCourse
+} = require("../controllers/Profile");
+const { auth } =require("../middilewares/auth");
 
 // delete user acc 
 
 router.delete("/deleteProfileUser", auth , deleteAccount);
 //get all users details
+
 router.get('/getAllUsersDetails', auth, getAllUserDetails)
 //update profile
+
 router.put("/updateProfile", auth, updateProfile)
 //upload display picture
+
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
 //get enrolled course of a student
+
 router.get("/getEnrollmentCourse" ,auth, getEnrolledCourse )
 
 module.exports=router;
